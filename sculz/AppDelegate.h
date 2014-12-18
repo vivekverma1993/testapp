14e0ba2191afe8f1ef95d8417ce800aaeba17e82
vivekverma1993 <vivekverma1993@gmail.com>
2014-12-18 22:38:18 +0530

    Initial Commit

---
+++ sculz/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sculz
+//
+//  Created by veddislabs on 18/12/2014.
+//  Copyright (c) 2014 self. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sculz/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sculz
+//
+//  Created by veddislabs on 18/12/2014.
+//  Copyright (c) 2014 self. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
